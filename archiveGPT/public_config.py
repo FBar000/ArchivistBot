592f4e42f7db4e603e86e78385c04930e3c11879
FBar000 <barrera.federico@outlook.com>
2024-01-13 16:58:32 -0500

    public config files

---
+++ archiveGPT/public_config.py
@@ -0,0 +1,19 @@
+API_KEY = r'' ## Include your API Key
+
+
+
+PRIMING_INSTRUCTION_PATH = r'Instructions'
+
+
+
+# Input/Output
+INPUT_FOLDER = r'Stage\\Input'
+OUTPUT_FOLDER = r'Stage\\Output'
+JSON_OUTPUT = r'Stage\\JSON_Output'
+
+
+# Archive
+IARCHIVE_FOLDER = r'Archive\\Input'
+OARCHIVE_FOLDER = r'Archive\\Output'
+JARCHIVE_FOLDER = r'Archive\\JSON'
+
---
+++ bots/public_configs.py
@@ -0,0 +1,57 @@
+CREDENTIALS = {
+    "username": "",
+    "password": ""
+} ## Add your CTCo Credentials
+
+# CTCo Variables
+
+URLS= {
+    "LOGIN_URL":  r"https://madison.ctcollections.org/index.php/system/auth/login",
+    "NEWDOC_URL": r'https://madison.ctcollections.org/editor/objects/ObjectEditor/Edit/type_id/27',
+    "SEARCH_BUILDER": r'https://madison.ctcollections.org/find/SearchObjectsBuilder/Index/reset/save'
+}
+
+
+
+LOGIN_XPATHS = {
+    "username": '//*[@id="login"]/div[1]/input',
+    "password": '//*[@id="login"]/div[2]/input',
+    "submit": ' //*[@id="login"]/div[3]/a'
+}
+
+DOC_MENU_XPATHS = {
+    "BASIC INFORMATION": '//*[@id="nav_screen_40"]',
+    "PHYSICAL CHARACTERISTICS": '//*[@id="nav_screen_41"]',
+    "CONDITION": '//*[@id="nav_screen_47"]',
+    "Save" : '//*[@aria-label="Save"]'
+}
+
+DOC_XPATHS = {
+    "idno": '//*[@id="idno_accession_number"]',
+    "title": '//*[@id="P732ObjectEditorForm_Prefname_new_0"]',
+    "date value": '//*[@id="P735ObjectEditorForm_attribute_56_57_new_0"]',
+    "date type": '//*[@id="P735ObjectEditorForm_attribute_56_58_new_0"]', 
+    "curatorial desc": '//*[@id="cke_1_contents"]/iframe'
+}
+
+DOC_PHYS_XPATHS = {
+    "height": '//*[@id="P742ObjectEditorForm_attribute_36_37_new_0"]',
+    "width": '//*[@id="P742ObjectEditorForm_attribute_36_38_new_0"]',
+    "measurement notes": '//*[@id="P742ObjectEditorForm_attribute_36_45_new_0"]'
+}
+
+DOC_COND_XPATHS = {
+    'condition notes': '(//textarea)[2]'
+}
+
+
+DATE_TYPE_CODES = {
+            "Date accepted": 182,
+            "Date collected": 185,
+            "Date copyrighted": 184,
+            "Date manufactured/created": 187,
+            "Date of alteration": 188,
+            "Date published": 183,
+            "Earliest/Latest dates": 186,
+            "Patent date": 189}
+
