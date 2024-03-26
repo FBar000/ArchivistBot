e48caa7e11d7c81bdba592b608d32b1a8e8572d7
FBar000 <barrera.federico@outlook.com>
2024-03-26 12:53:51 -0400

    adjusting file paths to reflect restructure

--- src/Archivist/archiveGPT/config.py
+++ src/Archivist/archiveGPT/config.py
@@ -7,13 +7,13 @@ PRIMING_INSTRUCTION_PATH = r'Instructions'
 
 
 # Input/Output
-INPUT_FOLDER = r'Stage\\Input'
-OUTPUT_FOLDER = r'Stage\\Output'
-JSON_OUTPUT = r'Stage\\JSON_Output'
+INPUT_FOLDER = r'src\\Material\\Stage\\Input'
+OUTPUT_FOLDER = r'src\\Material\\Stage\\Output'
+JSON_OUTPUT = r'src\\Material\\Stage\\JSON_Output'
 
 
 # Archive
-IARCHIVE_FOLDER = r'Archive\\Input'
-OARCHIVE_FOLDER = r'Archive\\Output'
-JARCHIVE_FOLDER = r'Archive\\JSON'
+IARCHIVE_FOLDER = r'src\\Material\\Archive\\Input'
+OARCHIVE_FOLDER = r'src\\Material\\Archive\\Output'
+JARCHIVE_FOLDER = r'src\\Material\\Archive\\JSON'
 
