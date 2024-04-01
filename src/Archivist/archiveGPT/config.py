9e19060bd5539dfd4f2e65797f72b9ff6a09d1ef
FBar000 <barrera.federico@outlook.com>
2024-04-01 15:26:38 -0400

    path name change

--- src/Archivist/archiveGPT/config.py
+++ src/Archivist/archiveGPT/config.py
@@ -6,7 +6,7 @@ PRIMING_INSTRUCTION_PATH = r'Archivist\archiveGPT\Instructions'
 # Input/Output
 INPUT_FOLDER = r'Material\\Stage\\Input'
 OUTPUT_FOLDER = r'Material\\Stage\\Output'
-JSON_OUTPUT = r'Material\\Stage\\JSON_Output'
+JSON_OUTPUT = r'Material\\Stage\\JSON'
 
 
 # Archive
