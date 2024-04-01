e69590781f7c3c8d7b678180bc8bf26e934a8234
FBar000 <barrera.federico@outlook.com>
2024-04-01 15:14:10 -0400

    corrected paths

--- src/Archivist/archiveGPT/config.py
+++ src/Archivist/archiveGPT/config.py
@@ -1,16 +1,16 @@
 
-PRIMING_INSTRUCTION_PATH = r'ArchivistBot\src\Archivist\archiveGPT\Instructions'
+PRIMING_INSTRUCTION_PATH = r'Archivist\archiveGPT\Instructions'
 
 
 
 # Input/Output
-INPUT_FOLDER = r'src\\Material\\Stage\\Input'
-OUTPUT_FOLDER = r'src\\Material\\Stage\\Output'
-JSON_OUTPUT = r'src\\Material\\Stage\\JSON_Output'
+INPUT_FOLDER = r'Material\\Stage\\Input'
+OUTPUT_FOLDER = r'Material\\Stage\\Output'
+JSON_OUTPUT = r'Material\\Stage\\JSON_Output'
 
 
 # Archive
-IARCHIVE_FOLDER = r'src\\Material\\Archive\\Input'
-OARCHIVE_FOLDER = r'src\\Material\\Archive\\Output'
-JARCHIVE_FOLDER = r'src\\Material\\Archive\\JSON'
+IARCHIVE_FOLDER = r'Material\\Archive\\Input'
+OARCHIVE_FOLDER = r'Material\\Archive\\Output'
+JARCHIVE_FOLDER = r'Material\\Archive\\JSON'
 
